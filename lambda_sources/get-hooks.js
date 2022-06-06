acc400c78dc9edf10e4a291187a262d2f92c4ee7
Kağan Üstüngel <kagan.ustungel@gmail.com>
2022-06-06 14:46:20 +0300

    new get hooks logic

--- lambda_sources/get-hooks.js
+++ lambda_sources/get-hooks.js
@@ -47,32 +47,49 @@ exports.handler = async (event, context) => {
     if (response.statusCode == 200) {
         let responseBody = JSON.parse(response.body);
 
-        let params = {
-            TableName: "tf_webhooks",
-            ExpressionAttributeValues: {
-                ":repo" : {S: `${user}/${repo}`}
-            },
-            KeyConditionExpression: "repo = :repo"
-        };
+        // we should have our relaying webhook registered on GitHub at this stage
+        if (responseBody.length > 0) {
 
-        try {
-            // query DynamoDB table to get terraform webhooks that we handle and add fake JSONs of them to the response
-            // so that terraform cloud thinks the webhook exists on GitHub and calls DELETE endpoint when necessary
-            const data = await ddb.query(params).promise();
-            data.Items.forEach(function(element, index, array) {
-                responseBody.push({
-                    type: "Repository",
-                    id: element.id.N,
-                    name: "web"
+            let params = {
+                TableName: "tf_webhooks",
+                ExpressionAttributeValues: {
+                    ":repo" : {S: `${user}/${repo}`}
+                },
+                KeyConditionExpression: "repo = :repo"
+            };
+
+            try {
+                // query DynamoDB table to get terraform webhooks that we handle and add fake JSONs of them to the response
+                // so that terraform cloud thinks the webhooks exists on GitHub and calls DELETE endpoint when necessary
+                const data = await ddb.query(params).promise();
+                data.Items.forEach(function(element, index, array) {
+                    let item = { ... responseBody[0] };
+
+                    item.id = element.id.N;
+                    // shallow copy, need to create a new config object not to overwrite real webhook data
+                    item.config = {
+                        content_type: "json",
+                        insecure_ssl: "0",
+                        secret: "********",
+                        url: element.url.S
+                    };
+                    item.created_at = element.date.S;
+                    item.updated_at = element.date.S;
+                    item.url = `https://api.github.com/repos/${element.repo.S}/hooks/${element.id.N}`;
+                    item.test_url = `https://api.github.com/repos/${element.repo.S}/hooks/${element.id.N}/test`;
+                    item.ping_url = `https://api.github.com/repos/${element.repo.S}/hooks/${element.id.N}/pings`;
+                    item.deliveries_url = `https://api.github.com/repos/${element.repo.S}/hooks/${element.id.N}/deliveries`;
+
+                    responseBody.push(item);
                 });
-            });
-            response.body = JSON.stringify(responseBody, null, 2);
-        } catch (err) {
-            console.log("Error", err);
-            response.body = JSON.stringify({
-                "error": `Could not list webhooks: ${err}`
-            }, null, 2);
-            response.statusCode = 500;
+                response.body = JSON.stringify(responseBody, null, 2);
+            } catch (err) {
+                console.log("Error", err);
+                response.body = JSON.stringify({
+                    "error": `Could not list webhooks: ${err}`
+                }, null, 2);
+                response.statusCode = 500;
+            }
         }
     }
 
